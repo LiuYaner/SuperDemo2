4ca2bfe6a27364aa0ac5af605e5da4eeadaa38c3
Pan Xiaoman <8722471930@qq.com>
2016-05-27 11:56:52 +0800

    双击后退键退出程序

--- app/src/main/java/com/libra/superdemo2/MainActivity.java
+++ app/src/main/java/com/libra/superdemo2/MainActivity.java
@@ -2,6 +2,7 @@ package com.libra.superdemo2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+
+
+    }
+    private  long lasttime;
+
+    @Override
+    public void onBackPressed() {
+        long ct=System.currentTimeMillis();
+        if (ct-lasttime>2000){
+            Toast.makeText(this,"再次点击后退见退出",Toast.LENGTH_LONG).show();
+            lasttime=ct;
+        }else {
+            super.onBackPressed();
+        }
+
     }
 }
